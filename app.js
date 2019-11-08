aa244ab900a59c8d6da3a4e01749b9fb358e4782
ajeet <mahi.aayush@gmail.com>
2019-11-08 14:40:59 +0530

    cours

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(cors());
+app.use(cors());
 
 // [SH] Initialise Passport before using the route middleware
 app.use(passport.initialize());
