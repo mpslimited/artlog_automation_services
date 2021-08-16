b5e8a97bf1192e8bb39ca22e8d2f6d6f2510668b
Gurpreet <mca2012gurpreetsingh@gmail.com>
2021-08-16 10:46:01 +0530

    angular demo build

--- log.js
+++
@@ -1,5 +0,0 @@
-const log4js = require('log4js');
-log4js.configure({
-  appenders: { cheese: { type: 'file', filename: 'logs/error.log' } },
-  categories: { default: { appenders: ['cheese'], level: 'error' } }
-});
