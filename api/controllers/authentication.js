014f86fbd9e14304a7449955b05ba08d56222abc
Gurpreet <mca2012gurpreetsingh@gmail.com>
2021-08-16 16:59:46 +0530

    cross issue

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -23,6 +23,10 @@ module.exports.register = function(req, res) {
 };
 
 module.exports.login = function(req, res) {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   console.log("REQ==>",req.body);
   passport.authenticate('local', function(err, user, info){
     var token;
