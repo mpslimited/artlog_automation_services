81b42fb94355dc97958193fbf6186a327a9ed1d2
Gurpreet <mca2012gurpreetsingh@gmail.com>
2021-08-17 12:12:29 +0530

    cross issue

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 // [SH] Initialise Passport before using the route middleware
 app.use(passport.initialize());
 // [SH] Use the API routes when path starts with /api
