775fbc8a5fc78ea0e65a43b185fb634bd19ff30c
Gurpreet <mca2012gurpreetsingh@gmail.com>
2021-08-17 13:36:57 +0530

    cross issue

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -32,11 +32,11 @@ module.exports.login = function(req, res) {
 
   let origin = req.headers.origin;
   // if (allowedOrigins.includes(origin)) {
-      res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+      res.header("Access-Control-Allow-Origin", '*'); // restrict it to the required domain
   // }
   // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   console.log("REQ==>",req.body);
   passport.authenticate('local', function(err, user, info){
     var token;
