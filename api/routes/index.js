95401e6adcb297698ee4627f8313cc7f508d2faf
Gurpreet <mca2012gurpreetsingh@gmail.com>
2021-08-18 17:18:49 +0530

    cross issue

--- api/routes/index.js
+++ api/routes/index.js
@@ -24,6 +24,18 @@ router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
 router.post('/login', ctrlAuth.login);
 router.route('/getUserInfo1',checkToken.checkToken ).post(function (req, res) {  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  
   res.send('Helloo    ');
 })
 
